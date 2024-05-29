53e50bf02206dcb933dcff2e6341f3128d7b4e91
Ludovic FAVRE ARTIGUES <ludovic.favreartigues@gmail.com>
2024-05-29 16:26:07 +0200

    email verification

--- data/verification-token.ts
+++ data/verification-token.ts
@@ -1,9 +1,9 @@
 import { db } from "@/lib/db";
 
-export const getVerificationTokenByEmail = async (email: string) => {
+export const getVerificationTokenByToken = async (token: string) => {
   try {
-    const verificationToken = await db.verificationToken.findFirst({
-      where: { email },
+    const verificationToken = await db.verificationToken.findUnique({
+      where: { token },
     });
     return verificationToken;
   } catch {
@@ -11,11 +11,12 @@ export const getVerificationTokenByEmail = async (email: string) => {
   }
 };
 
-export const getVerificationTokenByToken = async (token: string) => {
+export const getVerificationTokenByEmail = async (email: string) => {
   try {
-    const verificationToken = await db.verificationToken.findUnique({
-      where: { token },
+    const verificationToken = await db.verificationToken.findFirst({
+      where: { email },
     });
+
     return verificationToken;
   } catch {
     return null;
--- lib/tokens.ts
+++ lib/tokens.ts
@@ -7,7 +7,6 @@ export const generateVerificationToken = async (email: string) => {
   const expires = new Date(new Date().getTime() + 3600 * 1000);
 
   const existingToken = await getVerificationTokenByEmail(email);
-
   if (existingToken) {
     await db.verificationToken.delete({
       where: { id: existingToken.id },
